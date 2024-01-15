03221cfa280d8b6f223e6c2d701b693a25f7d82a
Tyler Bare <tylerbare98@gmail.com>
2024-01-15 17:01:36 -0600

    Initialization

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        
+        private readonly StoreContext _context;
+        
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+            
+        }
+
+        [HttpGet(Name = "GetProducts")]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
